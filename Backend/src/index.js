31fbdb45d382a19cdbd4f088526dfc07706c294c
warissayed <warissayed72@gmail.com>
2024-08-15 20:53:07 +0530

    creating a first approach using efe and async await for connecting db

---
+++ Backend/src/index.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+
+/*
+//first approach for creating backend and connecting mongodb and running on port 
+import express from "express";
+const app = express();
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("Error: ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is running on port ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("Error connecting to the database: ", error.message);
+    throw error;
+  }
+})();*/
